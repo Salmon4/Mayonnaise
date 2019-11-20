09bbb0821d7e2030b32d4bf9fb318052c183791d
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-11-20 14:10:01 -0500

    began copying code for login

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 
 app = Flask(__name__)
 
+app.secret_key = os.urandon(32)
+
+
+DB_FILE = "mayonnaise.db"
+
+db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
+c = db.cursor() #facilitate db operations
+
+def checkAuth():
+    if "userID" in session:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     app.debug = True;
     app.run()
-=======
 
->>>>>>> a935eb2cec539367f91f15e94aa82db4d8525de0
+
+
