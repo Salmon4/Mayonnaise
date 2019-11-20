de3c0c42aac0cb150d502efc7511a37b436bd33e
Albert Wan <albert@Alberts-MacBook-Air.local>
2019-11-20 13:54:34 -0500

    more html files and homepage code

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return render_template('homepage.html')
+
+
+@app.route("/createAccount")
+def createAccount():
+    return render_template("createAcc.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 if __name__ == "__main__":
     app.debug = True;
     app.run()
-=======
-
->>>>>>> a935eb2cec539367f91f15e94aa82db4d8525de0
