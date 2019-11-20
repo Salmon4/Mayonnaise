dd3b3abb7653809a64105b05d0da65cb57a34c0a
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-11-20 14:10:17 -0500

    added functions to create and setup tables

--- db.py
+++ db.py
@@ -8,10 +8,27 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
+def setup():
+	c.execute("CREATE TABLE IF NOT EXISTS users")
+	c.execute("CREATE TABLE IF NOT EXISTS topnews")
+	
+#new day: reset topnews table
+def resetnews():
+	c.execute("DROP TABLE IF EXISTS topnews")
+	c.execute("CREATE TABLE topnews")
+
+#deletes all existing tables
 def reset():
-    #remove tables to avoid dupliacte error
-    c.execute("DROP TABLE IF EXISTS users")
+    #LATER: create loop to delete every single user table
     c.execute("SELECT COUNT(1)")
     for i in range(users.
-    c.execute("DROP TABLE IF EXISTS students")
-    c.execute("DROP TABLE IF EXISTS stu_avg")
+    
+    #delete users and topnews table
+    c.execute("DROP TABLE IF EXISTS users")
+    c.execute("DROP TABLE IF EXISTS topnews")
+    
+#==========================================================
+def 
+    
+
+
