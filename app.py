864f63d8e6533c2d923760ebed4e8bb5205b82d6
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-11-20 21:32:44 -0500

    added auth and register paths

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3, os
-# from utl import db
+from utl import dbfunctions
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ DB_FILE = "mayonnaise.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
 c = db.cursor() #facilitate db operations
 
-# db.setup(c)
+dbfunctions.setup(c)
 
 def checkAuth():
     if "userID" in session:
@@ -33,6 +33,29 @@ def createAccount():
         redirect(url_for('home'))
     return render_template("createAcc.html")
 
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    password2 = request.form['password2']
+    c.execute("SELECT username FROM users WHERE username = ?", (username, ))
+    a = c.fetchone()
+    if a != None:
+        flash("Account with that username already exists")
+        return redirect(url_for('register'))
+    elif password != password2:
+        flash("Passwords do not match")
+        return redirect(url_for('register'))
+    elif len(password) < 8:
+        flash("Password must be at least 8 characters in length")
+        return redirect(url_for('register'))
+
+    else:
+        c.execute("INSERT INTO users VALUES (NULL, ?, ?)", (username, password))
+        db.commit()
+        flash("Successfuly created user")
+        return redirect(url_for('login'))
+
 @app.route("/login")
 def login():
     # if already logged in, don't display login page
@@ -40,6 +63,24 @@ def login():
         return redirect(url_for('home'))
     else:
         return render_template('login.html')
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    username = request.form['username']
+    password = request.form['password']
+    c.execute("SELECT userID, password FROM users WHERE username = ?", (username, ))
+    a = c.fetchone()
+    if a == None:
+        flash("No user found with given username")
+        return redirect(url_for('login'))
+    elif password != a[1]:
+        flash("Incorrect password")
+        return redirect(url_for('login'))
+    else:
+        session['userID'] = a[0]
+        session['username'] = username
+        flash("Welcome " + username + ". You have been logged in successfully.")
+        return redirect(url_for('home'))
 
 @app.route("/home")
 def home():
