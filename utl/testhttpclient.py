f2fb98bbbdd6c3c6613103d7616397c044db9d11
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-11-26 14:55:49 -0500

    continued testing"

--- utl/testhttpclient.py
+++ utl/testhttpclient.py
@@ -1,16 +1,16 @@
-# import http.client
-#
-# conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
-#
-# headers = {
-#     'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
-#     'x-rapidapi-key': "eedd51b020mshc462a1043ca26dep113106jsn43a6a1a8e712"
-#     }
-#
-# conn.request("GET", "/seasons/", headers=headers)
-#
-# res = conn.getresponse()
-# data = res.read()
+import http.client
+
+conn = http.client.HTTPSConnection("api-nba-v1.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+    'x-rapidapi-key': "eedd51b020mshc462a1043ca26dep113106jsn43a6a1a8e712"
+    }
+
+conn.request("GET", "/seasons/", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
 
 # import requests
 #
@@ -25,9 +25,9 @@
 #
 # print(response.text)
 
-response = unirest.get("https://api-nba-v1.p.rapidapi.com/seasons/",
-  headers={
-    "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": "eedd51b020mshc462a1043ca26dep113106jsn43a6a1a8e712"
-  }
-)
+# response = unirest.get("https://api-nba-v1.p.rapidapi.com/seasons/",
+#   headers={
+#     "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+#     "X-RapidAPI-Key": "eedd51b020mshc462a1043ca26dep113106jsn43a6a1a8e712"
+#   }
+# )
