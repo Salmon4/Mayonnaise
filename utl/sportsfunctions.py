bd3826a1db62cbacfea2d195cc103e562f28cada
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-11-29 10:19:18 -0500

    got function to add 3 scores from today if table is outdated or missing data

--- utl/sportsfunctions.py
+++ utl/sportsfunctions.py
@@ -1,6 +1,9 @@
-import sqlite3, json
+import sqlite3, json, datetime
 from utl import dbfunctions
 from urllib.request import urlopen
+
+d = datetime.datetime.today()
+date = d.strftime('%Y-%m-%d')
 
 #get array of teams added to preferences
 def getTeamsAdded(c, username):
@@ -25,6 +28,41 @@ def getUserTeamData(c, username, userteams, teamsdata):
         if team['name'] in userteams:
             out.append(team)
     return out
+
+#retreives scores from today, adds them to databse if today's data is incomplete
+# only adds 4 scores to database
+def getNHLTodayScores(c):
+    # c.execute("INSERT INTO nhl_scores VALUES(?, ?, ?, ?, ?)", ("s", 2, "d", "d", 1, 2))
+    c.execute("SELECT * FROM nhl_scores;")
+    scores = c.fetchall()
+    #print(len(scores))
+    #delete data from table if it's outdated:
+    # if len(scores) > 0 && scores[0][0].split("T")[0] != date:
+    if len(scores) > 0 and scores[0][0] != date:
+        c.execute("DELETE FROM nhl_scores;")
+    #api data:
+    u = urlopen("https://statsapi.web.nhl.com/api/v1/schedule")
+    response = u.read()
+    data = json.loads(response)
+    data = data['dates'][0]['games']
+    #only add to database if database is missing info
+    print(data)
+    if len(scores) < 3:
+        for game in data:
+            gamePk = game['gamePk']
+            print(gamePk)
+            #check if this gamePk is already in the table
+            if len(scores) == 0:
+                c.execute("INSERT INTO nhl_scores VALUES (?, ?, ?, ?, ?, ?)", (date, gamePk, game['teams']['home']['team']['name'],  game['teams']['away']['team']['name'], game['teams']['home']['score'], game['teams']['away']['score']))
+            else:
+                for tuple in scores:
+                    print(tuple)
+                    if gamePk not in tuple:
+                        c.execute("INSERT INTO nhl_scores VALUES (?, ?, ?, ?, ?, ?)", (date, gamePk, game['teams']['home']['team']['name'],  game['teams']['away']['team']['name'], game['teams']['home']['score'], game['teams']['away']['score']))
+    #by nowthe table is updated, so return data from the table.
+    c.execute("SELECT * FROM nhl_scores;")
+    scores = c.fetchall()
+    return scores
 
 #adds most recent game info to teamdata
 #parameter: data of user pref's teams (getUserTeamData)
